0d2f775b37e9a7056e6394c70c8e902ac825a758
Thamsanqa Mahlaola <mahlaola.thamsanqa@gmail.com>
2025-03-25 22:18:09 +0200

    Adding empty screens

---
+++ lib/ui/account_details_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AccountDetailsScreen extends StatelessWidget {
+  const AccountDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
