c32b10f50e233c6600f53d38e7b63b84d0775f23
Thamsanqa Mahlaola <mahlaola.thamsanqa@gmail.com>
2025-03-31 00:01:44 +0200

    code refactor

--- lib/api/model/github_user.dart
+++ lib/api/model/github_user.dart
@@ -1,23 +1,71 @@
-class GithubUser {
-  final String login;
+import 'package:hive/hive.dart';
+
+part 'github_user.g.dart';
+
+@HiveType(typeId: 0)
+class GithubUser extends HiveObject {
+
+  @HiveField(0)
   final int id;
-  final String nodeId;
+
+  @HiveField(1)
+  final String login;
+
+  @HiveField(2)
   final String avatarUrl;
+
+  @HiveField(3)
+  bool liked;
+
+  @HiveField(4)
+  final String nodeId;
+
+  @HiveField(5)
   final String gravatarId;
+
+  @HiveField(6)
   final String url;
+
+  @HiveField(7)
   final String htmlUrl;
+
+  @HiveField(8)
   final String followersUrl;
+
+  @HiveField(9)
   final String followingUrl;
+
+  @HiveField(10)
   final String gistsUrl;
+
+  @HiveField(11)
   final String starredUrl;
+
+  @HiveField(12)
   final String subscriptionsUrl;
+
+  @HiveField(13)
   final String organizationsUrl;
+
+  @HiveField(14)
   final String reposUrl;
+
+  @HiveField(15)
   final String eventsUrl;
+
+  @HiveField(16)
   final String receivedEventsUrl;
+
+  @HiveField(17)
   final String type;
+
+  @HiveField(18)
   final String userViewType;
+
+  @HiveField(19)
   final bool siteAdmin;
+
+  @HiveField(20)
   final double score;
 
   GithubUser({
@@ -41,6 +89,7 @@ class GithubUser {
     required this.userViewType,
     required this.siteAdmin,
     required this.score,
+    this.liked = false,
   });
 
   factory GithubUser.fromJson(Map<String, dynamic> json) {
@@ -64,7 +113,7 @@ class GithubUser {
       type: json['type'] as String,
       userViewType: json['user_view_type'] as String,
       siteAdmin: json['site_admin'] as bool,
-      score: (json['score'] != null ) ? (json['score'] as num).toDouble() : 0.0,
+      score: (json['score'] != null) ? (json['score'] as num).toDouble() : 0.0,
     );
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,22 @@ import 'package:github_profiles/api/client/authorization_interceptor.dart';
 import 'package:github_profiles/cubit/github_search_cubit.dart';
 import 'package:github_profiles/cubit/github_user_details_cubit.dart';
 import 'package:github_profiles/cubit/github_user_repos_cubit.dart';
+import 'package:github_profiles/storage/user_repository.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:github_profiles/router.dart';
 
 import 'api/client/github_client.dart';
+import 'api/model/github_user.dart';
 import 'api/service/github_service.dart';
 
-void main() {
-  const token = '';
+void main() async {
+  const token = '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';
   final dio = Dio()..interceptors.add(AuthorizationInterceptor(token: token));
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(GithubUserAdapter());
+
   runApp(GithubExplorerApp(service: GithubService(client: GithubClient(dio))));
 }
 
@@ -25,6 +33,9 @@ class GithubExplorerApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
+        RepositoryProvider(
+          create: (context) => UserRepository(),
+        ),
         BlocProvider(
           create: (context) => GithubSearchCubit(service: service),
         ),
@@ -33,7 +44,7 @@ class GithubExplorerApp extends StatelessWidget {
         ),
         BlocProvider(
           create: (context) => GithubUserReposCubit(service: service),
-        )
+        ),
       ],
       child: MaterialApp(
         title: 'Github Explorer',
